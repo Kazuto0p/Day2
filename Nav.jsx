176649f07781a3c11ede4f67085670590a71b423
Kazuto0p <122356200+Kazuto0p@users.noreply.github.com>
2024-04-03 15:59:27 +0530

    Add files via upload
    Day  2

---
+++ Nav.jsx
@@ -0,0 +1,24 @@
+import { AppBar, Toolbar,Button } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div>
+        <AppBar color='secondary'>
+            <Toolbar>
+                
+                <Button variant="contained"><Link to='/login' style={{color:'white',}} >Login</Link></Button>&nbsp;
+                <Button variant="contained"><Link to='/signup'style={{color:'white',}}>Signup</Link></Button>&nbsp;
+                <Button variant="contained"><Link to='/state'style={{color:'white',}}>State</Link></Button>&nbsp;
+                <Button variant="contained"><Link to='/color'>COLOR</Link></Button>
+                <Button variant="contained"><Link to='/value'>COUNTER</Link></Button>
+            </Toolbar>
+        </AppBar>
+        
+
+    </div>
+  )
+}
+
+export default Nav
